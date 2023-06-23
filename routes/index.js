459603287d1a84cfd8daebac1a492bf1230c278a
Kapilan <kapilan1996@gmail.com>
2023-06-23 16:11:15 +0200

    test

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/year', function(req, res, next) {
+ let currentYear = new Date().getFullYear();
+ res.json({year: currentYear.toString() }) ;
+});
+
+
+
+
+module.exports = router;
